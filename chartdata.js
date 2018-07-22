03b3f9ac0aa307df857f6ee0a200f5a3a7b6ed35
nakashimn <39855939+nakashimn@users.noreply.github.com>
2018-07-22 22:34:52 +0900

    Add files via upload

--- chartdata.js
+++ chartdata.js
@@ -1,35 +1,60 @@
+<!-- 処理変数 -->
 var updateTime_ms = 1000
 
-var ctx = document.getElementById("chart").getContext("2d");
-var x_label = new Array(120);
-x_label[0] = "0";
-x_label[59] = "1";
-x_label[119] = "2[min]";
-var data_label = "value";
-var data = new Array(120);
-for(var i=0;i<120;i++){
-  data[i] = (Math.random()+Math.random()+Math.random()+Math.random())/4;
+<!-- グラフ変数 -->
+var ctx = []
+for(var i=0;i<5;i++){
+  ctx.push(document.getElementById("chart"+"01").getContext("2d"))
 }
-var dataset = {label: data_label, data: data};
-window.onload = function(){
-  window.chart = new Chart(ctx,{
-    type: "line",
-    data: {
-      labels: x_label,
-      datasets: [dataset]
-    },
-    options: {
-      animation: false
-    }
-  });
-  setTimeout("updateChart()",updateTime_ms);
+var x_label = new Array(120)
+var data_label = "HeartRate[BPM]"
+var data = new Array(120)
+var dataset = {label: data_label, data: data}
+
+x_label[0] = "0"
+x_label[59] = "1"
+x_label[119] = "2[min]"
+
+<!-- 通信 & グラフ更新 -->
+var updateValue = function(){
+  $.ajax({
+    url: 'json/data.json',
+    type: 'get',
+    dataType: 'json'
+  })
+  .done(function(json){
+    chart.data.datasets.forEach(
+      dataset => {dataset.data.shift()
+                  dataset.data.push(json.ID00.Drowsiness)}
+  )
+  })
+  .fail(function(){
+    alert("failure")
+  })
+  chart.update()
+	setTimeout("updateValue()",updateTime_ms)
 }
 
-function updateChart(){
-  chart.data.datasets.forEach(
-    dataset => {dataset.data.shift();
-                dataset.data.push((Math.random()+Math.random()+Math.random()+Math.random())/4);}
-    );
-  chart.update();
-	setTimeout("updateChart()",updateTime_ms);
+<!-- グラフ初期化 -->
+window.onload = function(){
+    window.chart = new Chart(ctx[1],{
+      type: "line",
+      data: {
+        labels: x_label,
+        datasets: [dataset]
+      },
+      options: {
+        scales: {
+          yAxes:[{
+            ticks: {
+              beginAtZero: false,
+              min: 0,
+              max: 5
+            }
+          }]
+        }
+        animation: false
+      }
+    })
+  setTimeout("updateValue()",updateTime_ms)
 }
